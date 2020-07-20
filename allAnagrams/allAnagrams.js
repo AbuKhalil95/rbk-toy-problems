605f8ab6ceab0baef33b9462888fe32890e254bc
Abukhalil95 <yahya_abukhaleel@hotmail.com>
2020-07-20 04:40:58 +0300

    Attempted

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -15,6 +15,42 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
+ var allAnagrams = function(string) {
+  var possibleAnagrams= [];
+  var position = 0;
+  var onePossible = '';
 
+  console.log('length',string.length);
+  for (var i = 0; i < string.length; i++) {
+    console.log('first',i);
+
+    for (var j = 0; j < string.length-1; j++) {
+      console.log('variable',j);
+      onePossible += string[i];
+      position = j+1;
+      if (position > string.length) {
+        position = 0;
+      }
+      console.log('fixed',j+1);
+      var count = 1;
+
+      while (count < string.length) {
+        console.log('position',position);
+        if (position >= string.length) {
+          position = 0;
+        }
+        onePossible += string[position]
+        console.log('count',count);
+  
+        count++;
+        position ++;
+      } 
+      
+    possibleAnagrams.push(onePossible);
+    onePossible = '';
+    console.log(possibleAnagrams);
+    }
+  }
 };
+
+var anagrams = allAnagrams('abcde');
